8bbdf5519afd8bda9dc9094326913cb9a02b004f
hiroyukiuchiyama <uchiyamaworld@gmail.com>
2021-11-23 11:24:45 +0000

    画像投稿機完成作2

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -19,5 +19,8 @@ class PostImagesController < ApplicationController
   end
 
   def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
   end
 end
